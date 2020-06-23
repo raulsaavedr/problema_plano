e27b52b4fe13d2a6f87cf9f07390158983e3d4e4
raulsaavedr <raulsaavedr@gmail.com>
2020-06-23 13:40:27 -0400

    Deleting the old versions of code.

--- src/0.1.5/problema_plano/matlab/m_gauss.m
+++
@@ -1,5 +0,0 @@
-function [A, b, matriz] = m_gauss()
-% Comparacion de solucion A\b matriz gauss - matlab
-A = csvread('../csv/ejemplo/salida/matriz_coeficientes.csv');
-b = csvread('../csv//ejemplo/vectores_valores_dependientes.csv');
-matriz = A\b;
